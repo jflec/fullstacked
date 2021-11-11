1503002876e892189fa27623ef1cad9d30a166ef
JFlec <josephflec@gmail.com>
2021-11-10 18:34:32 -0600

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
 require 'rest-client'
 require 'json'
-require 'byebug'
 
 response = RestClient.get 'http://ddragon.leagueoflegends.com/cdn/11.22.1/data/en_US/champion.json'
 champions_arr = JSON.parse(response)["data"]
-# debugger
 champions_arr.each do |champions|
     Champion.create(
         name: champions[1]["name"],
